f4b811719bb324c505508d830371909d0b7c33ea
Aarush Verma <aarushverma20007@gmail.com>
2026-06-20 16:07:21 +0530

    Added user details

---
+++ submissions/AarushVerma07/user.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#Participant to enter their details here:
+
+name="Aarush Verma"
+Roll_no="16010125273"
+branch="Computer Engineering"
+mail="aarush14@somaiya.edu"
+phone="8057993075"
+remarks="overthewire solved only till level 13"
+#enter anything you want to add
+
+echo $name " | " $Roll_no " | " $branch " | " $remarks
+echo $mail - $phone
